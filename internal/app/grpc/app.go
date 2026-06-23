42dd07163df80d994c9e2066fc639eabd2e0e7dd
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-06-23 18:02:16 +0300

    add app

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,51 @@
+package grpcapp
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log/slog"
+	"net"
+	"sso/internal/grpc/auth"
+)
+
+type App struct {
+	log        *slog.Logger
+	GRPCServer *grpc.Server
+	port       int
+}
+
+func New(
+	log *slog.Logger,
+	port int,
+) *App {
+	gRPCServer := grpc.NewServer()
+
+	auth.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		GRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+
+	log := a.log.With(slog.String("op", op), slog.Int("port", a.port))
+
+	log.Info("starting gRPC server")
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("gRPC server is running", slog.String("address", l.Addr().String()))
+
+	if err := a.GRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
