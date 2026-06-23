5cecc096657a206aaa4ca3d2e5ccfc39cb29af53
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2026-06-23 17:29:35 +0300

    add logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,14 +1,45 @@
-package sso
+package main
 
-import "sso/internal/config"
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
-	// TODO: init config object
 	cfg := config.MustLoad()
 
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
 
+	log.Info("starting application", slog.String("env", cfg.Env))
 	// TODO: init app
 
 	// TODO: gRPC server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
